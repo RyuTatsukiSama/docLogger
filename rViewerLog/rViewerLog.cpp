320030e8cdabe0d8c80dbeb4ae5ea1de34f58dd4
RyuTatsukiSama <dorianribault@gmail.com>
2025-08-20 17:11:26 +0200

    rViewerLog.cpp

--- rViewerLog/rViewerLog.cpp
+++ rViewerLog/rViewerLog.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-#include <filesystem>
+#include <fstream>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        std::cerr << "Usage: cat <fichier>\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file)
+    {
+        std::cerr << "Impossible d'ouvrir : " << argv[1] << "\n";
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+
+    system("pause");
+
     return 0;
 }
