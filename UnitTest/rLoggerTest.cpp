87cb8ea2c3d827531059fdf464f18c3bd4cecbc3
RyuTatsukiSama <dorianribault@gmail.com>
2025-08-26 21:31:34 +0200

    delete rtesting, using " logCallbacks " to test FormatLog

--- UnitTest/rLoggerTest.cpp
+++ UnitTest/rLoggerTest.cpp
@@ -1,17 +1,28 @@
 #include <gtest/gtest.h>
-#define RTESTING
 #include "../src/rLogger/rLogger.h"
+
+#pragma region FormatLogTest
+
+static std::string formatedMessage;
+
+void FormatLogTester(std::string _formatedMessage)
+{
+    formatedMessage = _formatedMessage;
+}
 
 TEST(rLoggerTest, FormatLogTest)
 {
     auto fixedTime = std::chrono::system_clock::from_time_t(0);
     rLogger testLogger(rLoggerOptions::make({.timeProvider = [=]
                                              { return fixedTime; }}));
+    testLogger.RegisterLogCallback(FormatLogTester);
 
     std::string tester = std::format("| [TRACE] [{:%Y-%m-%d %H:%M:%S}] [Main] This is a trace level |", fixedTime);
-    std::string format = testLogger.GetFormatLog(rLoggerSeverity::Trace, "This is a trace level");
-    EXPECT_EQ(format, tester);
+    testLogger.Log(rLoggerSeverity::Trace, "This is a trace level");
+    EXPECT_EQ(formatedMessage, tester);
 }
+
+#pragma endregion
 
 TEST(rLoggerTest, CallerTest)
 {
