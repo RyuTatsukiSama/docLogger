50a0ef674968c9866a6399fd80ce7357fec191e4
RyuTatsukiSama <dorianribault@gmail.com>
2025-08-27 15:10:21 +0200

    write in file unit test

--- UnitTest/rLoggerTest.cpp
+++ UnitTest/rLoggerTest.cpp
@@ -61,8 +61,32 @@ TEST(rLoggerTest, SeverityFuncTest)
 
 TEST(rLoggertest, WriteFileTest)
 {
-}
+    std::chrono::system_clock::time_point fixedTime = std::chrono::system_clock::from_time_t(0);
+    rLogger logger(rLoggerOptions::make({.outputConsole = false,
+                                         .fileName = "unit_test",
+                                         .timeProvider = [=]
+                                         { return fixedTime; }}));
 
-TEST(rLoggertest, ColorTest)
-{
+    logger.Trace("Log");
+    logger.Debug("Log");
+    logger.Info("Log");
+    logger.Warning("Log");
+    logger.Error("Log");
+    logger.Critical("Log");
+
+    std::fstream file("rLogs/unit_test.log", std::ios::in);
+
+    std::string line = "";
+    std::getline(file, line);
+    EXPECT_EQ(line, "\033[35m| [TRACE] [1970-01-01 00:00:00.0000000] [Main] Log |\033[0m");
+    std::getline(file, line);
+    EXPECT_EQ(line, "\033[34m| [DEBUG] [1970-01-01 00:00:00.0000000] [Main] Log |\033[0m");
+    std::getline(file, line);
+    EXPECT_EQ(line, "\033[32m| [INFO] [1970-01-01 00:00:00.0000000] [Main] Log |\033[0m");
+    std::getline(file, line);
+    EXPECT_EQ(line, "\033[33m| [WARNING] [1970-01-01 00:00:00.0000000] [Main] Log |\033[0m");
+    std::getline(file, line);
+    EXPECT_EQ(line, "\033[31m| [ERROR] [1970-01-01 00:00:00.0000000] [Main] Log |\033[0m");
+    std::getline(file, line);
+    EXPECT_EQ(line, "\033[97;41m| [CRITICAL] [1970-01-01 00:00:00.0000000] [Main] Log |\033[0m");
 }
--- src/TaskManager/taskManager.cpp
+++ src/TaskManager/taskManager.cpp
@@ -13,6 +13,29 @@ void anotherFunc(void)
 
 int main()
 {
+	/*std::chrono::system_clock::time_point fixedTime = std::chrono::system_clock::from_time_t(0);
+	rLogger logger(rLoggerOptions::make({.outputConsole = true,
+										 .fileName = "unit_test",
+										 .timeProvider = [=]
+										 { return fixedTime; }}));
+
+	logger.Trace("Log");
+
+	std::fstream file("rLogs/unit_test.log", std::ios::in);
+
+	std::string reader = "";
+	std::string line = "";
+	while (!file.eof())
+	{
+		file >> reader;
+		line += " " + reader;
+		if (reader == "|[0m")
+		{
+			std::cout << line << " and [35m| [TRACE] [1970-01-01 00:00:00.0000000] [Main] Log |[0m" << std::endl;
+			std::cout << (line == "[35m| [TRACE] [1970-01-01 00:00:00.0000000] [Main] Log |[0m") << std::endl;
+		}
+	}*/
+
 	rLogger log(rLoggerOptions::defaults);
 
 	log.Caller();
@@ -24,8 +47,7 @@ int main()
 	log.Log(rLoggerSeverity::Error, "This is a Error test");
 	log.Log(rLoggerSeverity::Critical, "This is a Critical test");
 
-	/*
-	std::thread t1(threadFunc, "t1");
+	/*std::thread t1(threadFunc, "t1");
 	t1.join();
 
 	std::thread t2(threadFunc, "t2");
