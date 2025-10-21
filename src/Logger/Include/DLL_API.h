c8c2d846afb989d3012d91beb0402a6f7f17f6e6
RyuTatsukiSama <dorianribault@gmail.com>
2025-10-21 22:06:31 +0200

    delete useless code seen that the static lib has been removed

--- src/Logger/Include/DLL_API.h
+++ src/Logger/Include/DLL_API.h
@@ -3,8 +3,6 @@
 
 #if defined(_WIN32) && defined(CMAKEDLL_EXPORTS)
 #define DLL_API __declspec(dllexport)
-#elif defined(CMAKELIB_EXPORTS)
-#define DLL_API
 #else
 #define DLL_API __declspec(dllimport)
 #endif
