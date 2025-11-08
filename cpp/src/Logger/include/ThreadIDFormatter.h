de87f1f9a35c4e838ed62a05d286572ddb31d5cf
RyuTatsukiSama <dorianribault@gmail.com>
2025-11-08 13:34:27 +0100

    Add ThreadIDFormatter.h
    Use for std::format to work with std::thread::id

---
+++ cpp/src/Logger/include/ThreadIDFormatter.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <format>
+#include <thread>
+#include <sstream>
+
+template <>
+struct std::formatter<std::thread::id>
+{
+    constexpr auto parse(std::format_parse_context &ctx)
+    {
+        return ctx.begin();
+    }
+
+    auto format(const std::thread::id &id, std::format_context &ctx) const
+    {
+        std::ostringstream oss;
+        oss << id;
+        return std::format_to(ctx.out(), "{}", oss.str());
+    }
+};
