43f33035073a3aecf2b034e02833fabb77ea465a
RyuTatsukiSama <dorianribault@gmail.com>
2025-11-10 12:04:15 +0100

    LoggerOption struct

--- go/docLogger/LoggerOptions.go
+++ go/docLogger/LoggerOptions.go
@@ -1,4 +1,14 @@
 package docLogger
 
+import (
+	"io"
+	"time"
+)
+
 type LoggerOptions struct {
+	isOutputConsole bool
+	isOutputFile    bool
+	fileName        string
+	fileStream      *io.Writer
+	timeProvider    func() time.Time
 }
