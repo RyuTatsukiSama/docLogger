3c2dba853a0257274d38a347090d826a3f2f8d17
RyuTatsukiSama <dorianribault@gmail.com>
2025-11-10 12:03:36 +0100

    LoggerSeverity struct and level

--- go/docLogger/LoggerSeverity.go
+++ go/docLogger/LoggerSeverity.go
@@ -1,4 +1,15 @@
 package docLogger
 
 type LoggerSeverity struct {
+	value int
 }
+
+var (
+	Trace    = LoggerSeverity{value: 0}
+	Debug    = LoggerSeverity{value: 1}
+	Info     = LoggerSeverity{value: 2}
+	Warning  = LoggerSeverity{value: 3}
+	Error    = LoggerSeverity{value: 4}
+	Critical = LoggerSeverity{value: 5}
+	None     = LoggerSeverity{value: 6}
+)
