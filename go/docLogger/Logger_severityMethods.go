2f1a6a301c78744927447bef1e3616492bcad11a
RyuTatsukiSama <dorianribault@gmail.com>
2026-01-09 09:55:24 +0100

    add severity methods

--- go/docLogger/Logger_severityMethods.go
+++ go/docLogger/Logger_severityMethods.go
@@ -1,1 +1,25 @@
 package docLogger
+
+func (this *Logger) Trace(message string) {
+	this.Log(Trace, message)
+}
+
+func (this *Logger) Debug(message string) {
+	this.Log(Debug, message)
+}
+
+func (this *Logger) Info(message string) {
+	this.Log(Info, message)
+}
+
+func (this *Logger) Warning(message string) {
+	this.Log(Warning, message)
+}
+
+func (this *Logger) Error(message string) {
+	this.Log(Error, message)
+}
+
+func (this *Logger) Critical(message string) {
+	this.Log(Critical, message)
+}
