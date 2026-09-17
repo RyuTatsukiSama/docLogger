1ce5c3719d169e9cb23eb440e045a3e30c2fb814
RyuTatsukiSama <dorianribault@gmail.com>
2026-09-17 23:47:14 +0200

    fix import/export dll non-windows

--- cpp/src/Logger/include/doc/DLL_API.h
+++ cpp/src/Logger/include/doc/DLL_API.h
@@ -1,10 +1,14 @@
 #ifndef DLLAPI_H
 #define DLLAPI_H
 
-#if defined(_WIN32) && defined(CMAKEDLL_EXPORTS)
-#define DLL_API __declspec(dllexport)
+#if defined(_WIN32)
+    #if defined(CMAKEDLL_EXPORTS)
+        #define DLL_API __declspec(dllexport)
+    #else
+        #define DLL_API __declspec(dllimport)
+    #endif
 #else
-#define DLL_API __declspec(dllimport)
+    #define DLL_API
 #endif
 
 #endif
